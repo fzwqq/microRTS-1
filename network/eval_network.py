a1b9a8f0cd126a3be9e2f225b4e719a89f9c7ef7
amoy_zhp <whitehawk15@hotmail.com>
2019-04-28 20:48:21 +0800

    complete network structure

---
+++ network/eval_network.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.layers import LeakyReLU
+
+class LeakyReLU(LeakyReLU):
+    def __init__(self, **kwargs):
+        self.__name__ = "LeakyReLU"
+        super(LeakyReLU, self).__init__(**kwargs)
+
+#Hyper parameter
+LEARKYRELU_ALPHA = -1/5.5
+
+
+
+class EvalNetworkModel(keras.Model):
+
+    def __init__(self):
+        super(EvalNetworkModel,self).__init__(name='')
+        self.drop_out = keras.layers.Dropout(rate=0.2)
+        self.LReLU = LeakyReLU(alpha=LEARKYRELU_ALPHA)
+        self.flatten = keras.layers.Flatten()
+        self.conv2d_1 = keras.layers.Conv2D(filters=32, kernel_size=2, strides=(1,1), 
+                        padding='valid', activation=self.LReLU)
+
+        self.conv2d_2 = keras.layers.Conv2D(filters=32, kernel_size=3, strides=(2,2), 
+                        padding='same', activation=self.LReLU, name='conv_2')
+
+        self.conv2d_3 = keras.layers.Conv2D(filters=48, kernel_size=2, strides=(1,1), 
+                        padding='valid', activation=self.LReLU, name='conv_3')
+
+        self.conv2d_4 = keras.layers.Conv2D(filters=48, kernel_size=3, strides=(2,2), 
+                        padding='same', activation=self.LReLU, name='conv_4')
+
+        self.conv2d_5 = keras.layers.Conv2D(filters=64, kernel_size=2, strides=(1,1), 
+                        padding='valid', activation=self.LReLU, name='conv_5')
+
+        self.conv2d_6 = keras.layers.Conv2D(filters=64, kernel_size=3, strides=(2,2),
+                        padding='same', activation=self.LReLU, name='conv_6')
+
+        self.conv2d_7 = keras.layers.Conv2D(filters=64, kernel_size=1, strides=(1,1), 
+                        padding='same', activation=self.LReLU, name='conv_7')
+
+        self.conv2d_8_eval = keras.layers.Conv2D(filters=2, kernel_size=1, strides=(1,1), 
+                        padding='same', activation=self.LReLU)
+    
+        self.global_average_pool = keras.layers.GlobalAveragePooling2D()
+        self.dense_9_eval = keras.layers.Dense(2, activation='softmax')
+
+    def conv_drop(self, conv_layer, input_tensor):
+        output = conv_layer(input_tensor)
+        output = self.drop_out(output)
+        return output
+
+    def call(self, input_tensor):
+        output_1 = self.conv_drop(self.conv2d_1, input_tensor)
+        output_2 = self.conv_drop(self.conv2d_2, output_1)
+        output_3 = self.conv_drop(self.conv2d_3, output_2)
+        output_4 = self.conv_drop(self.conv2d_4, output_3)
+        output_5 = self.conv_drop(self.conv2d_5, output_4)
+        output_6 = self.conv_drop(self.conv2d_6, output_5)
+        output_7 = self.conv_drop(self.conv2d_7, output_6)
+        output_8_eval = self.global_average_pool(self.conv_drop(self.conv2d_8_eval, output_7))
+        eval_output = self.dense_9_eval(self.flatten(output_8_eval))
+        return eval_output
+
+def create_model():
+    return EvalNetworkModel()
